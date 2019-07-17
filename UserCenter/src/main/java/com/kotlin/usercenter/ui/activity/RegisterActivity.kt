1b517260f15d22fd26a08f5903f5069be7b585db
wangh <wangh1@tritonsfs.com>
2019-07-17 14:32:16 +0800

    引入anko 库

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -3,11 +3,17 @@ package com.kotlin.usercenter.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.kotlin.usercenter.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnLogin.setOnClickListener {
+//            Toast.makeText(this,"!!!!",Toast.LENGTH_SHORT).show()
+            toast("登录")
+        }
     }
 }
